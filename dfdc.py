46fc5cd7fae090dda7ac901dc9b6644d7f3e25da
Zeshan <141054421+zzesshhaann@users.noreply.github.com>
2023-09-08 17:38:01 +0500

    Add files via upload

--- dfdc.py
+++ dfdc.py
@@ -7,6 +7,8 @@ Original file is located at
     https://colab.research.google.com/drive/1ePfluDse6CUmHUgCakx1fCsdw7ibHYbF
 """
 
+pip install opencv-python
+
 import streamlit as st
 import cv2
 import numpy as np
